6983bde1aca5ccaf8d8736f1a33d9791d4ddf32f
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-03-11 09:24:27 -0700

    211. Create orders (repo & repo impl).

---
+++ lib/core/repos/orders_repo/orders_repo_impl.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../features/checkout/data/models/order_model.dart';
+import '../../../features/checkout/domain/entities/order_entity.dart';
+import '../../errors/failures.dart';
+import '../../services/database_service.dart';
+import '../../utils/end_points.dart';
+import 'orders_repo.dart';
+
+class OrdersRepoImpl implements OrdersRepo {
+  final DatabaseService service;
+
+  OrdersRepoImpl({required this.service});
+
+  @override
+  Future<Either<Failure, void>> addOrder({required OrderEntity order}) async {
+    try {
+      await service.addData(
+        path: EndPoints.addOrders,
+        data: OrderModel.fromEntity(order).toJson(),
+      );
+      return Right(null);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+}
--- lib/core/utils/end_points.dart
+++ lib/core/utils/end_points.dart
@@ -2,6 +2,6 @@ abstract class EndPoints {
   static const String addUsersData = 'Users';
   static const String getUsersData = 'Users';
   static const String isUserExists = 'Users';
-
   static const String getProduct = "Products";
+  static const String addOrders = "Orders";
 }
