afeea4517d89c4931264c03ee4ff18e3001cd0bf
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-03-24 09:53:05 -0700

    complete-order-view done.

--- lib/features/checkout/presentation/views/complete_order_view.dart
+++ lib/features/checkout/presentation/views/complete_order_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/utils/app_images.dart';
+import '../../../../core/utils/app_text_styles.dart';
 import '../../../../core/widgets/custom_app_bar.dart';
+import '../../../../core/widgets/custom_button.dart';
 
 class CompleteOrderView extends StatelessWidget {
   const CompleteOrderView({super.key});
@@ -14,6 +17,40 @@ class CompleteOrderView extends StatelessWidget {
         title: 'الطلب',
         showNotifications: false,
         showBack: false,
+      ),
+      body: CompleteOrderViewBody(),
+    );
+  }
+}
+
+class CompleteOrderViewBody extends StatelessWidget {
+  const CompleteOrderViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          const Spacer(),
+          SizedBox(
+            height: 200,
+            width: 200,
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: Image.asset(Assets.imagesOrderCompletedIcon),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Text('تم الطلب بنجاح!', style: AppTextStyles.bold19),
+          const Spacer(flex: 2),
+          CustomButton(
+            text: 'الذهاب للرئيسية',
+            onPressed: () =>
+                Navigator.popUntil(context, (route) => route.isFirst),
+          ),
+          const Spacer(flex: 2),
+        ],
       ),
     );
   }
